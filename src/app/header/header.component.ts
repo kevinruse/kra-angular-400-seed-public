cf60ec64a06f59fd2cdcdb00a6edfe9b9fb8476b
kevinruse <kevin@kevinruse.com>
2020-01-06 14:00:36 -0800

    build a component for the seed app - challenge exercise 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,17 @@
-/**
- * Created by Kevin Ruse + Associates Inc. on 1/6/2020.
- */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    template: `<header>
+                       <img src="assets/images/icons/seed-icon.png" alt="">
+                       <h1>Welcome to Angular Seed!</h1>
+                  </header>`
+})
+
+export class HeaderComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
