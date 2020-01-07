53b11f2ac21815cf83a5379e06d1b7238624151b
kevinruse <kevin@kevinruse.com>
2020-01-07 10:16:44 -0800

    style angular component - guided exercise 8 styling angular components with a component style block

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
-})
+    templateUrl: './header.component.html',
+    })
 
 export class HeaderComponent implements OnInit {
     constructor() {
